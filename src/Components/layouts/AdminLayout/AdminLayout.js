ad3a26e44bc9f2627d6653f2cf5cf7db9a7d8cf2
nguyenhuuduc64 <duchuuiq@gmail.com>
2025-03-01 23:31:11 +0700

    fix adminLayout

--- src/Components/layouts/AdminLayout/AdminLayout.js
+++ src/Components/layouts/AdminLayout/AdminLayout.js
@@ -1,10 +1,12 @@
 import Sidebar from "../SidebarComponents/Sidebar/sidebar";
 import classNames from "classnames/bind";
 import styles from "./AdminLayout.module.scss"; // Import SCSS
+import Header from "../Header/header";
 
 const cx = classNames.bind(styles);
 
 function AdminLayout({ children }) {
+  <Header />;
   return (
     <div className={cx("admin-layout")}>
       <Sidebar />
